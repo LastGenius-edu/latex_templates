aaef93dbb845a74bf15d7a2e4177fcf7590ceea3
LastGenius-edu <as.andreysultanov@gmail.com>
2021-03-24 14:10:49 +0200

    Fix a little typo in week2 presentation

--- rust_2/code/option4.rs
+++ rust_2/code/option4.rs
@@ -1,6 +1,6 @@
 let res = divide(2.0, 3.0);
 
-// if `let` destructures `optional` into
+// if `let` destructures `res` into
 // `Some(i)`, evaluate the block (`{}`).
 if let Some(x) = res {
     println!("Result: {}", x);
--- rust_2/code/option5.rs
+++ rust_2/code/option5.rs
@@ -1,6 +1,6 @@
 let res = divide(2.0, 3.0);
 
-// while `let` destructures `optional` into
+// while `let` destructures `res` into
 // `Some(i)`, evaluate the block (`{}`). Else `break`.
 while let Some(x) = res {
     println!("Result: {}", x);
